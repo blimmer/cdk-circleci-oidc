b951aee6d4588f97d684edcbd31403d46b26e725
Ben Limmer <ben@benlimmer.com>
2024-04-05 08:10:37 -0600

    chore: update projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,5 @@
-const { awscdk } = require('projen');
+import { awscdk } from 'projen';
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Ben Limmer',
   authorAddress: 'hello@benlimmer.com',
@@ -7,6 +8,10 @@ const project = new awscdk.AwsCdkConstructLibrary({
   name: '@blimmer/cdk-circleci-oidc',
   repositoryUrl: 'https://github.com/blimmer/cdk-circleci-oidc.git',
 
+  projenrcTs: true,
+
+  jsiiVersion: "~5.0.0",
+
   python: {
     distName: 'cdk-circleci-oidc',
     module: 'cdk_circleci_oidc',
@@ -14,9 +19,12 @@ const project = new awscdk.AwsCdkConstructLibrary({
 
   depsUpgrade: false,
 
+  prettier: true,
+
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
 project.synth();
